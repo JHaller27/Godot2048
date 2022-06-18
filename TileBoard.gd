713aa975f55f76ad79ad9d225c73812d4a340c11
James <jameshaller27@gmail.com>
2022-06-17 22:42:11 -0700

    Bugfix slide left

--- TileBoard.gd
+++ TileBoard.gd
@@ -50,6 +50,17 @@ func _find_empty_spot() -> Array:
 	return free_tiles[rng.randi_range(0, free_tiles.size()-1)]
 
 
+func add_tile(row: int, col: int, val: int) -> void:
+	var new_tile = tile_scene.instance()
+	new_tile.set_position(positions[row][col])
+	new_tile.set_scale(scale)
+
+	tiles[row][col] = new_tile
+	new_tile.set_value(val)
+
+	self.add_child(new_tile)
+
+
 func add_random_tile() -> bool:
 	var index = _find_empty_spot()
 	if index.size() == 0:
@@ -58,22 +69,18 @@ func add_random_tile() -> bool:
 	var row = index[0]
 	var col = index[1]
 
-	var new_tile = tile_scene.instance()
-	new_tile.set_position(positions[row][col])
-	new_tile.set_scale(scale)
-
-	tiles[row][col] = new_tile
 	var value = 1
 	if rng.randi_range(0, 9) >= 9:
 		value = 2
-	new_tile.set_value(value)
-
-	self.add_child(new_tile)
+	self.add_tile(row, col, value)
 
 	return true
 
 
 func _slide_tile(from_obj: Object, from_y: int, from_x: int, to_obj: Object, to_y: int, to_x: int):
+	if to_obj != null && from_obj.get_value() == to_obj.get_value():
+		self.new_values.append([to_y, to_x, to_obj.get_value()+1])
+
 	var from_pos = self.positions[from_y][from_x]
 	var to_pos = self.positions[to_y][to_x]
 	self.tiles[to_y][to_x] = self.tiles[from_y][from_x]
@@ -82,41 +89,6 @@ func _slide_tile(from_obj: Object, from_y: int, from_x: int, to_obj: Object, to_
 
 	if to_obj != null:
 		self.to_free.append(to_obj)
-
-
-func _find_next_left_tile(dst_tile: Object, dst_index: int, row: Array) -> int:
-	for src_index in range(dst_index+1, row.size()):
-		var src_tile = row[src_index]
-
-		# If dst is empty, look for a non-empty tile
-		if dst_tile == null:
-			if src_tile != null:
-				return src_index
-
-		# If dst is not empty, look for an identical tile
-		else:
-			if src_tile != null:
-				if src_tile.get_value() == dst_tile.get_value():
-					return src_index
-				return -1
-
-	return -1
-
-
-func _slide_left():
-	for row_index in range(self.tiles.size()):
-		var row = self.tiles[row_index]
-		var dst_index = 0
-		while dst_index < row.size():
-			var dst_tile = row[dst_index]
-			var src_index = _find_next_left_tile(dst_tile, dst_index, row)
-			if src_index != -1:
-				var src_tile = row[src_index]
-				self._slide_tile(src_tile, row_index, src_index, dst_tile, row_index, dst_index)
-				if dst_tile != null:
-					self.new_values.append([row_index, dst_index, src_tile.get_value()+1])
-			dst_index += 1
-	$Tween.start()
 
 
 func _on_done_tweening():
@@ -134,9 +106,57 @@ func _on_done_tweening():
 	self.add_random_tile()
 
 
+func _find_next_left_spot(src_tile: Object, src_index: int, row: Array) -> int:
+	var dst_index = src_index
+	for scout_index in range(src_index-1, -1, -1):
+		dst_index = scout_index+1
+		var scout_tile = row[scout_index]
+		if scout_tile == null:
+			continue
+
+		var dst_tile = row[dst_index]
+		if scout_tile.get_value() == src_tile.get_value():
+			return scout_index
+		else:
+			return dst_index
+
+	return 0
+
+
+func _slide_left():
+	for row_index in range(self.tiles.size()):
+		var row = self.tiles[row_index]
+		for src_index in range(1, row.size()):
+			var src_tile = row[src_index]
+			if src_tile == null:
+				continue
+			var dst_index = _find_next_left_spot(src_tile, src_index, row)
+			if dst_index != src_index:
+				self._slide_tile(src_tile, row_index, src_index, row[dst_index], row_index, dst_index)
+	$Tween.start()
+
+
+func _slide_right():
+	pass
+
+
+func _slide_up():
+	pass
+
+
+func _slide_down():
+	pass
+
+
 func _input(event):
 	if event.is_action_pressed("slide_left"):
 		self._slide_left()
+	elif event.is_action_pressed("slide_right"):
+		self._slide_right()
+	elif event.is_action_pressed("slide_up"):
+		self._slide_up()
+	elif event.is_action_pressed("slide_down"):
+		self._slide_down()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
