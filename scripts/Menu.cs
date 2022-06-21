7b73d9c4ae4e24717f58f9f1613c6110cd24a377
James <jameshaller27@gmail.com>
2022-06-20 23:32:32 -0700

    Remove all GDScript and start C#

---
+++ scripts/Menu.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class Menu : Control
+{
+	private void _on_PlayButton_pressed()
+	{
+		// Replace with function body.
+	}
+
+
+	private void _on_QuitButton_pressed()
+	{
+		this.GetTree().Quit();
+	}
+
+
+	private void _on_SaveButton_pressed()
+	{
+		// Replace with function body.
+	}
+
+
+	private void _on_LoadButton_pressed()
+	{
+		// Replace with function body.
+	}
+}
